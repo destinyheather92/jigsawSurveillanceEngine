a502f0d0a93078c430106888856d296ec9f81f4a
Destiny Heather Mills <dhmills292@gmail.com>
2026-05-29 18:15:41 -0400

    reset prisma migrations for postgres

--- server/prisma/migrations/20260529221416_init_postgres/migration.sql
+++ server/prisma/migrations/20260529221416_init_postgres/migration.sql
@@ -1,19 +1,23 @@
 -- CreateTable
 CREATE TABLE "Scenario" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "choices" TEXT NOT NULL
+    "choices" TEXT NOT NULL,
+
+    CONSTRAINT "Scenario_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Attempt" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "scenarioId" TEXT NOT NULL,
     "choice" TEXT NOT NULL,
     "outcome" TEXT NOT NULL,
     "survivalScore" INTEGER NOT NULL,
     "moralScore" INTEGER NOT NULL,
     "message" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Attempt_pkey" PRIMARY KEY ("id")
 );
